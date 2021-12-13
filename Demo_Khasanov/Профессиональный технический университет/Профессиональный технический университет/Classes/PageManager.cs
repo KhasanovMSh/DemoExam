01dc90007984ac62727e176c3c14a3c08c286715
KhasanovMSh <nooblohotron@gmail.com>
2021-12-13 16:00:35 +0300

    Add files via upload

---
+++ Demo_Khasanov/Профессиональный технический университет/Профессиональный технический университет/Classes/PageManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Профессиональный_технический_университет.Classes
+{
+    public class PageManager
+    {
+        public static Frame MainFrame;
+    }
+}
